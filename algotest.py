633abf75e2a3c607bee5be66008dd1d0a8c78190
Dorus van Schaik <dorusvschaik@gmail.com>
2022-01-24 20:08:31 +0100

    Changes in algotest.py to make algorithm work

--- algotest.py
+++ algotest.py
@@ -1,42 +1,45 @@
 from spca import Spca
-from elasticnet import ElasticNet
+# from elasticnet import ElasticNet
+from sklearn.linear_model import ElasticNet
+from sklearn.datasets import make_regression
 import pandas as pd
 import numpy as np
 
 def main():
-    # Import pitprops data
     pitprops = pd.read_csv("data/pitprops.csv", index_col=0) 
-    eigenvalues = np.linalg.eig(pitprops)
-    firstloadings = eigenvalues[0]
-    
-    #normalized_result = Spca.SPCAalgo(firstloadings)
-    #print(normalized_result)
+    eigenvalues, eigenvectors = np.linalg.eig(pitprops)
+    eigenvalues = eigenvalues[0:6]
+    eigenvectors = eigenvectors[0:6, 0:6]
 
+    Csqrt = (eigenvectors * eigenvalues**0.5) @ eigenvectors.T
+    
     difference = 10 # Intial diff has to > threshold
-    threshold = 0.1 # Random default value
+    threshold = 3.8 # Random default value
     i = 0 # Initialize i
     k = 6 # Default value from paper
-    maxit = 10000 # Random default value
-    lambda1 = np.array[0.06, 0.16, 0.1, 0.5, 0.5, 0.5] # Default value from paper
-   
-    A =  np.zeros((maxit, len(firstloadings)))
-    A[0] = firstloadings
+    maxit = 600 # Random default value
+    lambda1 = np.array([0.06, 0.16, 0.1, 0.5, 0.5, 0.5]) # Default value from paper
 
-    while difference < threshold:
-        B = np.zeros((maxit, len(firstloadings))) # initialise B (10000,13)
-        
+    A =  np.zeros((maxit, k))
+    Atemp = eigenvectors
+
+    B = np.zeros((606, 6))
+    B_temp = np.zeros((6,k))
+
+    while difference > threshold and i < maxit:
         for j in range(0,k):
-            #B[i][j] = ElasticNet.enetoptimizer(self, 1, A[i], maxit, lambda1)( A[j] )
-            
-            #print(B[i][j])
-
-        U, D, vh = np.linalg.svd(pitprops.iloc[0:6,0:6]*B[i], full_matrices=True)
-        A[i+1] = U @ np.transpose(vh)
-        difference = np.linalg.norm(A[i+1]-A[i]) # Difference of two vectors? Or sum of differences over each corresponding element?
+            elastic_net_solver = ElasticNet(alpha=1, l1_ratio=lambda1[j], fit_intercept=False, max_iter=1e6).fit(Csqrt, Csqrt @ Atemp[:, 1])
+            B_temp[j] = np.array(elastic_net_solver.coef_)
+        B[i:(i+k)] = B_temp
+        U, D, vh = np.linalg.svd(pitprops.iloc[0:6,0:6]*pitprops.iloc[0:6,0:6]*B[i], full_matrices=True)
+        difference = np.linalg.norm((U @ np.transpose(vh))-Atemp)
+        Atemp = (U @ np.transpose(vh))
         i += 1
-        #print(B[i][j])
+            
+    print("Optimization terminated succesfully \n")
 
-    normalized_loadings = B / np.linalg.norm(B)
+    print("Normalized Loadings")
+    normalized_loadings = B[i:(i+6)] / np.linalg.norm(B[i:(i+6)])
     print(normalized_loadings)
 
     # Open questions:
@@ -48,5 +51,5 @@ def main():
     # Correlation matrix * vector 
     # (20*13)(13*1) # First 6 loadings or all 13 loadings, how to subset the 
 
-if _name_ == '_main_':
+if __name__ == '__main__':
     main()
