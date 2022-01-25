914da52918d1592b86c17134d92033b097879186
Dorus van Schaik <dorusvschaik@gmail.com>
2022-01-25 16:36:29 +0100

    Fixed l1 and l2 norm, data dimensions, A and B difference and sorting of eigenvalues/vectors

--- algotest.py
+++ algotest.py
@@ -1,17 +1,9 @@
-from spca import Spca
-# from elasticnet import ElasticNet
 from sklearn.linear_model import ElasticNet
 from sklearn.datasets import make_regression
 import pandas as pd
 import numpy as np
 
-# To-Do
-# - Order eigenvalues and eigenvectors
-# - Change loadings from 6 to 13
-# - Check whether difference should be based on B, A and B or only A
-# - Check calculation of lambda's
-#
-# Then;
+# To-do after results are correct
 # - Fix code layout (i.e. transfer algotest.py to notebook and class)
 # - PEP8
 # - Implement gene dataset
@@ -20,32 +12,43 @@ import numpy as np
 def main():
     pitprops = pd.read_csv("data/pitprops.csv", index_col=0) 
     eigenvalues, eigenvectors = np.linalg.eig(pitprops)
-    eigenvalues = eigenvalues[0:6]
-    eigenvectors = eigenvectors[0:6, 0:6]
+
+    eigenvalues = eigenvalues.argsort()[::-1]
+    eigenvectors = eigenvectors.argsort()[::-1]
+
+    eigenvalues = eigenvalues
+    eigenvectors = eigenvectors
 
     Csqrt = (eigenvectors * eigenvalues**0.5) @ eigenvectors.T
-    
-    difference = 10 # Intial diff has to > threshold
-    threshold = 3.8 # Random default value
+
+    difference = 1 # Intial diff has to > threshold
+    difference_b = 1
+    threshold = 1e-6 # Random default value
     i = 0 # Initialize i
     k = 6 # Default value from paper
-    maxit = 600 # Random default value
+    maxit = 1000 # Random default value
     lambda1 = np.array([0.06, 0.16, 0.1, 0.5, 0.5, 0.5]) # Default value from paper
+    lambda1 = lambda1
 
-    A =  np.zeros((maxit, k))
-    Atemp = eigenvectors
+    Atemp = eigenvectors[:, :k]
 
-    B = np.zeros((606, 6))
-    B_temp = np.zeros((6,k))
+    B = np.zeros((10600, 13))
+    B_temp = np.zeros((k,13))
 
-    while difference > threshold and i < maxit:
+    #while difference > threshold and difference_b > threshold and i < maxit:
+    while i < maxit:
         for j in range(0,k):
-            elastic_net_solver = ElasticNet(alpha=1, l1_ratio=lambda1[j], fit_intercept=False, max_iter=1e6).fit(Csqrt, Csqrt @ Atemp[:, 1])
+            elastic_net_solver = ElasticNet(alpha=lambda1[j], l1_ratio=0.5, fit_intercept=False, max_iter=1e6).fit(Csqrt, Csqrt @ Atemp[:, j])
             B_temp[j] = np.array(elastic_net_solver.coef_)
         B[i:(i+k)] = B_temp
-        U, D, vh = np.linalg.svd(pitprops.iloc[0:6,0:6]*pitprops.iloc[0:6,0:6]*B[i], full_matrices=True)
+
+        U, D, vh = np.linalg.svd((pitprops.T @ pitprops) @ np.transpose(B[i:(i+k)]))
+        U =  U[:, :k]
+        vh = vh[:k]
         difference = np.linalg.norm((U @ np.transpose(vh))-Atemp)
+        difference_b = 1 if i < 10 else np.linalg.norm((B[i:(i+k)])-(B[(i-6):(i+k-6)]))
         Atemp = (U @ np.transpose(vh))
+        print(difference_b)
         i += 1
             
     print("Optimization terminated succesfully \n")
@@ -53,15 +56,6 @@ def main():
     print("Normalized Loadings")
     normalized_loadings = B[i:(i+6)] / np.linalg.norm(B[i:(i+6)])
     print(normalized_loadings)
-
-    # Open questions:
-    # Eerste 6 loadings? 6x13 of 6x1?
-    # Hoe SVD van geniedata in equation 3 berekenen?
-    # Which version of equation 3 to use?
-    # SVD = np.linalg.svd((X.T @ X) @ B) # How to define X? Grab from elasticnet? Is part of the elasticnet equation, but also X^t@X is the covariance matrix?
-    # SVD = np.linalg.svd(U @ D @ np.transpose(V)) # = np.transpose(X) @ X @ B
-    # Correlation matrix * vector 
-    # (20*13)(13*1) # First 6 loadings or all 13 loadings, how to subset the 
 
 if __name__ == '__main__':
     main()
